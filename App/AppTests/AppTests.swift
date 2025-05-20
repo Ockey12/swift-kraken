13104196cbc7cb495a425d54cac85b140dafad01
Ockey12 <haloupgarou@gmail.com>
2025-05-21 01:08:19 +0900

    create xcworkspace

---
+++ App/AppTests/AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by Ockey on 2025/05/21.
+//
+
+import Testing
+@testable import App
+
+struct AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
