926570d976b2f2800eac5bb1c92944a5b42ab3f4
Ockey12 <83934125+Ockey12@users.noreply.github.com>
2025-09-07 10:57:39 +0900

    create project

---
+++ swift-kraken-app/swift-kraken-app/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  swift-kraken-app
+//
+//  Created by Ockey on 2025/09/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
