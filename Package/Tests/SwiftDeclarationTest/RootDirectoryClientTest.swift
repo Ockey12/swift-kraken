aa86deba76e723918142f1f7e57f581a1f8346e0
Ockey12 <83934125+Ockey12@users.noreply.github.com>
2025-09-14 23:26:44 +0900

    add getSwiftDeclaration test

--- Package/Tests/SwiftDeclarationTest/RootDirectoryClientTest.swift
+++ Package/Tests/SwiftDeclarationTest/RootDirectoryClientTest.swift
@@ -73,60 +73,63 @@ func rootDirectoryClientTest() async throws {
     let filePathInRootDirectory = rootDirectoryURL.appending(component: "FileInRootDirectory.swift").path()
 
     let countID = UUID(uuidString: "00000000-0000-0000-0000-000000000002")!
+    let countLocationRange = Location(
+        fullPath: filePathInRootDirectory,
+        line: 12,
+        column: 5,
+    )
+        ... Location(
+            fullPath: filePathInRootDirectory,
+            line: 12,
+            column: 18,
+        )
     let countUSR = USR("s:8TestData16CounterViewModelC5countSivp")
     let count = AbstractDeclaration(
         id: countID,
         name: "count",
         kind: .variable,
-        sourceLocationRange: Location(
-            fullPath: filePathInRootDirectory,
-            line: 12,
-            column: 5,
-        )
-            ... Location(
-                fullPath: filePathInRootDirectory,
-                line: 12,
-                column: 18,
-            ),
+        sourceLocationRange: countLocationRange,
         definitionUSRs: [countUSR],
     )
 
     let incrementID = UUID(uuidString: "00000000-0000-0000-0000-000000000003")!
+    let incrementLocationRange = Location(
+        fullPath: filePathInRootDirectory,
+        line: 14,
+        column: 5,
+    )
+        ... Location(
+            fullPath: filePathInRootDirectory,
+            line: 16,
+            column: 6,
+        )
     let incrementUSR = USR("s:8TestData16CounterViewModelC9incrementyyF")
     let increment = AbstractDeclaration(
         id: incrementID,
         name: "increment",
         kind: .function,
-        sourceLocationRange: Location(
-            fullPath: filePathInRootDirectory,
-            line: 14,
-            column: 5,
-        )
-            ... Location(
-                fullPath: filePathInRootDirectory,
-                line: 16,
-                column: 6,
-            ),
+        sourceLocationRange: incrementLocationRange,
         definitionUSRs: [incrementUSR],
     )
 
     let counterViewModelID = UUID(uuidString: "00000000-0000-0000-0000-000000000001")!
+    let counterViewModelLocationRange = Location(
+        fullPath: filePathInRootDirectory,
+        line: 10,
+        column: 1,
+    )
+        ... Location(
+            fullPath: filePathInRootDirectory,
+            line: 17,
+            column: 2,
+        )
     let counterViewModelUSR = USR("s:8TestData16CounterViewModelC")
     let counterViewModelInitUSR = USR("s:8TestData16CounterViewModelCACycfc")
     let counterViewModel = AbstractDeclaration(
         id: counterViewModelID,
         name: "CounterViewModel",
         kind: .class,
-        sourceLocationRange: Location(
-            fullPath: filePathInRootDirectory,
-            line: 10,
-            column: 1,
-        )
-            ... Location(
-                fullPath: filePathInRootDirectory,
-                line: 17,
-                column: 2,
-            ),
+        sourceLocationRange: counterViewModelLocationRange,
         definitionUSRs: [
             counterViewModelUSR,
             counterViewModelInitUSR,
@@ -160,6 +163,70 @@ func rootDirectoryClientTest() async throws {
         abstractDeclarations: [counterViewModel],
     )
 
+    let expectedRootDirectory = RootDirectory(
+        directory: Directory(
+            fullPath: rootDirectoryPath,
+            subDirectories: [subDirectory],
+            files: [fileInRootDirectory],
+        ),
+        keyPathTable: KeyPathTable(
+            directories: [
+                rootDirectoryPath: \.self,
+                subDirectoryPath: \.?.subDirectories[id: subDirectoryPath],
+            ],
+            files: [
+                filePathInRootDirectory: \.?.files[id: filePathInRootDirectory],
+                filePathInSubDirectory: \.?.subDirectories[id: subDirectoryPath]?.files[id: filePathInSubDirectory],
+            ],
+            abstractDeclarations: [
+                counterViewModelUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID],
+                counterViewModelInitUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID],
+                countUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID]?.variables[id: countID],
+                incrementUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID]?.functions[id: incrementID],
+                subCounterViewModelUSR: \.?.subDirectories[id: subDirectoryPath]?.files[id: filePathInSubDirectory]?.abstractDeclarations[id: subCounterViewModelID],
+            ],
+        ),
+    )
+
+    // MARK: SwiftDeclaration
+
+    let countDeclaration = VariableDeclaration(
+        id: countID,
+        name: "count",
+        sourceLocationRange: countLocationRange,
+        variables: [],
+        functions: [],
+        nestingStructs: [],
+        nestingClasses: [],
+        nestingEnums: [],
+    )
+
+    let incrementDeclaration = FunctionDeclaration(
+        id: incrementID,
+        name: "increment",
+        sourceLocationRange: incrementLocationRange,
+        variables: [],
+        functions: [],
+        nestingStructs: [],
+        nestingClasses: [],
+        nestingEnums: [],
+    )
+
+    let counterViewModelDeclaration = ClassDeclaration(
+        id: counterViewModelID,
+        name: "CounterViewModel",
+        sourceLocationRange: counterViewModelLocationRange,
+        variables: [countDeclaration],
+        functions: [incrementDeclaration],
+        nestingStructs: [],
+        nestingClasses: [],
+        nestingEnums: [],
+    )
+
+    let expectedDeclaration = SwiftDeclaration.class(counterViewModelDeclaration)
+
+    // MARK: withDependencies
+
     try await withDependencies {
         $0.uuid = .incrementing
         $0.rootDirectoryClient = .liveValue
@@ -168,31 +235,9 @@ func rootDirectoryClientTest() async throws {
         @Dependency(\.rootDirectoryClient) var rootDirectoryClient
         let response = try await rootDirectoryClient.extract(rootDirectoryURL: rootDirectoryURL, indexStoreURL: indexStoreURL)
 
-        let expectedResult = RootDirectory(
-            directory: Directory(
-                fullPath: rootDirectoryPath,
-                subDirectories: [subDirectory],
-                files: [fileInRootDirectory],
-            ),
-            keyPathTable: KeyPathTable(
-                directories: [
-                    rootDirectoryPath: \.self,
-                    subDirectoryPath: \.?.subDirectories[id: subDirectoryPath],
-                ],
-                files: [
-                    filePathInRootDirectory: \.?.files[id: filePathInRootDirectory],
-                    filePathInSubDirectory: \.?.subDirectories[id: subDirectoryPath]?.files[id: filePathInSubDirectory],
-                ],
-                abstractDeclarations: [
-                    counterViewModelUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID],
-                    counterViewModelInitUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID],
-                    countUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID]?.variables[id: countID],
-                    incrementUSR: \.?.files[id: filePathInRootDirectory]?.abstractDeclarations[id: counterViewModelID]?.functions[id: incrementID],
-                    subCounterViewModelUSR: \.?.subDirectories[id: subDirectoryPath]?.files[id: filePathInSubDirectory]?.abstractDeclarations[id: subCounterViewModelID],
-                ],
-            ),
-        )
+        expectNoDifference(expectedRootDirectory, response)
 
-        expectNoDifference(expectedResult, response)
+        let counterViewModel = try #require(response.getSwiftDeclaration(withUSR: counterViewModelUSR))
+        expectNoDifference(counterViewModel, expectedDeclaration)
     }
 }
